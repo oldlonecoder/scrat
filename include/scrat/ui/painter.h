e793d8635ea77308874bf2c20f07fed7f24553f0
oldlonecoder <lussier.serge@gmail.com>
2022-12-30 16:20:25 -0500

    begin adding painter and ui flags

---
+++ include/scrat/ui/painter.h
@@ -0,0 +1,68 @@
+#pragma once
+
+#include <scrat/ui/vdc.h>
+#include <scrat/ui/uidefs.h>
+
+namespace scrat::ui
+{
+
+class SCRAT_API painter
+{
+    vdc* _dc = nullptr;
+    rect _r;   ///< Geometrie interne. (Celui du _dc ou un sous-rectangle de _dc)
+    vdc::cell _cell;
+    vdc::cell::type* cursor = nullptr;
+    bool _wrap = false;
+
+    point cpos();
+    Justify::Type j_bits = 0;
+    _decl_object
+
+public:
+
+    painter() = default;
+    ~painter() = default;
+
+    painter(vdc* aOwner, rect aR = rect::Nil);
+    painter(painter&&) = default;
+    painter(const painter&) = default;
+
+    painter& operator = (painter&&) = default noexcept;
+    painter& operator = (const painter&) = default;
+
+
+
+    painter& operator << (color::type aColorID);
+    painter& operator << (Icon::Type aIconID);
+    painter& operator << (const std::string& aStr);
+    painter& operator << (const String& aStr);
+    painter& operator << (const point& XY);
+    painter& operator << (const char* aStr);
+    painter& operator << (Accent::Type aAcc);
+
+
+    painter& GotoXY(const point& XY);
+    painter& Home() { return GotoXY({}); }
+
+    template<typename T> painter& operator << (const T& aStr)
+    {
+        std::ostringstream Out;
+        Out << aStr;
+        return (*this) << Out.str();
+    }
+
+    painter& set_colors(const textattr::pair& pair_);
+
+    result<> setup_geometry();
+    painter& clear();
+   // void Update();
+    Justify::Type& justify_bits() { return j_bits; }
+    painter& set_bits(Justify::Type aBits);// { Bits = aBits; return *this; }
+    painter& set_hcenter(bool S);// { Bits = S ? (Bits & ~HCenter) | HCenter : (Bits & ~HCenter); return *this; }
+    painter& set_vcenter(bool S);
+    painter& set_center(bool S);
+    painter& set_word_wrap(bool S);
+    painter& set_bg(color::type aBg);
+    painter& set_fg(color::type aFg);
+};
+}
---
+++ include/scrat/ui/uidefs.h
@@ -0,0 +1,53 @@
+#pragma once
+#include <cstdint>
+
+namespace scrat::ui
+{
+
+namespace Justify
+{
+using Type = uint8_t;
+static constexpr uint8_t HCenter = 0x01;
+static constexpr uint8_t VCenter = 0x02;
+static constexpr uint8_t Center = 0x03;
+static constexpr uint8_t WordWRap = 0x08;
+static constexpr uint8_t Prefix = 0x10;
+static constexpr uint8_t Suffix = 0x20;
+}
+
+namespace Direction
+{
+using Type = int8_t;
+static constexpr int8_t Left = -1;
+static constexpr int8_t Right = 1;
+static constexpr int8_t Up = 2;
+static constexpr int8_t Down = -2;
+}
+
+
+namespace WClass
+{
+using Type = uint16_t;
+static constexpr Type TopLevel = 0x0001;
+static constexpr Type Floating = 0x0002;
+static constexpr Type Child = 0x0004;
+static constexpr Type Parent = 0x0008;
+static constexpr Type Caption = 0x0010;
+static constexpr Type Frame = 0x0020;
+static constexpr Type Input = 0x0040;
+
+}
+// ...
+namespace State
+{
+	using Type = uint8_t;
+	static constexpr Type Normal  = 0x00;
+	static constexpr Type Disable = 0x01;
+	static constexpr Type Active  = 0x02;
+	static constexpr Type Focus   = 0x04;
+	static constexpr Type Error   = 0x08;
+	static constexpr Type Warning = 0x10;
+	static constexpr Type Illegal = 0x20;
+}
+}
+
